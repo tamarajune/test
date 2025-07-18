c7ea22b338ef2359d9693e694dcc027a81a1ae9e
tamarajune <tamks.email@gmail.com>
2025-07-18 11:17:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,12 @@ print(abyss.greet())
 while True:
     user = input("You: ")
     print("Abyss:", abyss.respond(user))
+from abyss_agent import AbyssAgent
+
+abyss = AbyssAgent()
+
+while True:
+    user = input("You: ")
+    if user.lower() in ["quit", "exit"]:
+        break
+    print("Abyss:", abyss.respond(user))
